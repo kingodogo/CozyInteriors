cd4901253c5dad3387ee8f5488525c155780d368
Kingooo <kingodogo@outlook.com>
2026-01-03 19:04:30 +0530

    Add Versions/1.18.2/src/main/java/com/kingodogo/cozyinteriors/CozyInteriors.java

---
+++ Versions/1.18.2/src/main/java/com/kingodogo/cozyinteriors/CozyInteriors.java
@@ -0,0 +1,35 @@
+package com.kingodogo.cozyinteriors;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(CozyInteriors.MOD_ID)
+public class CozyInteriors {
+
+    public static final String MOD_ID = "cozyinteriors";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public CozyInteriors() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+        LOGGER.info("CozyInteriors initialized");
+    }
+
+    @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class RegistryEvents {
+        @SubscribeEvent
+        public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
+        }
+    }
+}
+
